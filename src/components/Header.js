7484dce2220144124a6706448d61e34b44e68e75
Weiyu Zhao <136862333+wyzhaobvt@users.noreply.github.com>
2023-11-13 22:51:28 -0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
             <Link to='/'><img src="/img/logo.png" className="img-fluid m-2 rounded" width='40px' alt=""/></Link>
             </div>
             <div className='col-sm-5 text-center'>
-                <p className='m-0 tagline'>Welcome! Your real estate destination!</p>
+                <p className='m-0 tagline'>Welcome!!! Your real estate destination!</p>
             </div>
             <div className='col-sm-4 d-flex justify-content-end'>
             {(localStorage.getItem('custname'))?
